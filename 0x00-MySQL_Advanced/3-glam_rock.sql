7a078355acf84877e8301394f9e6ba51b1bba346
NafisaKaruri <nafeesahassan016@gmail.com>
2024-10-16 23:55:11 +0000

    task 3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,12 @@
+-- Lists all bands with `Glam rock` as their main style,
+-- ranked by their longevity
+select band_name,
+(
+    CASE
+        WHEN split IS NULL THEN 2022 - formed
+        ELSE split - formed
+    END
+) AS lifespan
+FROM metal_bands
+WHERE style = 'Glam rock'
+ORDER BY lifespan DESC;
