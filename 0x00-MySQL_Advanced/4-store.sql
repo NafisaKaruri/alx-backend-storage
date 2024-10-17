bc08cb382b59574effdc1e42ca7bda0cf585e18a
NafisaKaruri <nafeesahassan016@gmail.com>
2024-10-17 00:33:11 +0000

    task 4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,7 +1,12 @@
 -- Creates a trigger that decreases the quality of an item after adding a new order.
+DROP TRIGGER IF EXISTS order_trigger;
+DELIMITER $$
 CREATE TRIGGER order_trigger
 AFTER INSERT ON orders
 FOR EACH ROW
+BEGIN
 	UPDATE items
-	SET quality = quality - NEW.number
+	SET quantity = quantity - NEW.number
 	WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
