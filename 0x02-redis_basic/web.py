07205ccf83cb14cf1efb25fc809270747421d853
NafisaKaruri <nafeesahassan016@gmail.com>
2024-10-23 22:27:06 +0000

    get_page

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+Web cache and tracker using Redis.
+"""
+import redis
+import requests
+from functools import wraps
+from typing import Callable
+
+
+redis_client = redis.Redis()
+
+
+def cache_page(method: Callable) -> Callable:
+    """
+    Decorator to cache the results of a method based on the URL.
+
+    Args:
+        method (Callable): The method to cache.
+
+    Returns:
+        Callable: The wrapped function with caching behavior.
+    """
+    @wraps(method)
+    def invoker(url: str) -> str:
+        """
+        Invokes the method and caches the result.
+
+        Args:
+            url (str): The URL to fetch.
+
+        Returns:
+            str: The HTML content of the page.
+        """
+        redis_client.incr(f'count:{url}')
+
+        cached_result = redis_client.get(f'result:{url}')
+        if cached_result:
+            return cached_result.decode('utf-8')
+
+        result = method(url)
+
+        redis_client.setex(f'result:{url}', 10, result)
+        return result
+
+    return invoker
+
+
+@cache_page
+def get_page(url: str) -> str:
+    """
+    Fetches the HTML content of a given URL.
+
+    Args:
+        url (str): The URL of the page to retrieve.
+
+    Returns:
+        str: The HTML content of the page.
+    """
+    return requests.get(url).text
